8140ba3eec4b248ea6ff4d4c084bac323401941a
Foley <KFoley@usbr.gov>
2019-02-01 10:30:40 -0700

    install to user directory, not system

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -4,31 +4,31 @@ ECHO.
 ECHO.
 ECHO Installing python libraries
 ECHO.
-pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org numpy -U
+pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org numpy -U --user
 ECHO O
-pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org scipy -U
+pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org scipy -U --user
 ECHO OO
-pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org requests -U
+pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org requests -U --user
 ECHO OOO
-pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org zeep -U
+pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org zeep -U --user
 ECHO OOOO
-pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org pandas -U
+pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org pandas -U --user
 ECHO OOOOO
-pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org sklearn -U
+pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org sklearn -U --user
 ECHO OOOOOO
-pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org matplotlib -U
+pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org matplotlib -U --user
 ECHO OOOOOOO
-pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org -Iv PyQt5==5.9
+pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org -Iv PyQt5==5.9.2 --user
 ECHO OOOOOOOO
-pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org datetime -U
+pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org datetime -U --user
 ECHO OOOOOOOOO
-pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org PyQt5-tools -U
+
 ECHO OOOOOOOOOO
-pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org openpyxl -U
+pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org openpyxl -U --user
 ECHO OOOOOOOOOOO
-pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org configparser -U
+pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org configparser -U --user
 ECHO OOOOOOOOOOO
-pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org xlrd -U
+pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org xlrd -U --user
 ECHO OOOOOOOOOOOO
 ECHO.
 ECHO Press any key to exit
