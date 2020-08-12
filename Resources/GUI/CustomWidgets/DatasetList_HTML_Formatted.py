ed3d1396bf1e4187ee25cfa193e8fa5cdd5a2de8
dloney <dloney@usbr.gov>
2020-08-12 13:56:00 -0600

    Corrected signal error when passing pandas series instead of dataframe

--- Resources/GUI/CustomWidgets/DatasetList_HTML_Formatted.py
+++ Resources/GUI/CustomWidgets/DatasetList_HTML_Formatted.py
@@ -237,7 +237,7 @@ class DatasetList_HTML_Formatted(QtWidgets.QListWidget):
             self.setItemWidget(item, widget)
 
         # Send the signal to update other objects that reference this list, passing the updated dataframe
-        self.updateSignal.emit(self.datasetTable)
+        self.updateSignal.emit(pd.DataFrame(self.datasetTable))
 
         return
 
