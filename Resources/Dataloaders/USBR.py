f1887bca1c2b01c14e29eb0de03785867cfc473b
Bob Lounsbury <blounsbury@usbr.gov>
2026-04-06 16:03:14 -0600

    Fix chained string comparison bug.
    * if ('a' or 'b' in 'xy') evaluates to if ('a') or ('b' in 'xy'). This will always return true no matter what 'xy' is, because if 'a' is always True.

--- Resources/Dataloaders/USBR.py
+++ Resources/Dataloaders/USBR.py
@@ -1,3 +1,5 @@
+import requests
+from io import StringIO
 import pandas as pd
 from datetime import datetime
 
@@ -9,7 +11,7 @@ class Dataloader(object):
 
   def load(self, dataset, date1, date2):
 
-    if 'MB' or 'GP' in dataset.agency:
+    if 'MB' in dataset.agency or 'GP' in dataset.agency:
 
       url = ('https://www.usbr.gov/gp-bin/webarccsv.pl?'
              f'parameter={dataset.external_id}%20{dataset.param_code}&'
@@ -24,7 +26,7 @@ class Dataloader(object):
 
       return df.iloc[:, 0]
     
-    elif 'PN' or 'CPN' in dataset.agency:
+    elif 'PN' in dataset.agency or 'CPN' in dataset.agency:
 
         # Download instructions for daily PN data:
         url = ('https://www.usbr.gov/pn-bin/daily.pl?'
@@ -34,7 +36,7 @@ class Dataloader(object):
                f'format=csv')
 
         # Download instructions for monthly PN data:
-        if 'monthly2daily' in opts:
+        if 'monthly2daily' in dataset.opts:
             url = ('https://www.usbr.gov/pn-bin/monthly.pl?'
                    f'parameter={dataset.external_id}%20{dataset.param_code}&'
                    f'start={datetime.strftime(date1, '%Y-%m-%d')}&'
@@ -53,11 +55,11 @@ class Dataloader(object):
         df.set_index(pd.DatetimeIndex(pd.to_datetime(df['DateTime'])), inplace=True) # Set the index to the datetime column
         del df['DateTime'] # Delete the redundant datetime column
         df = df[~df.index.duplicated(keep='first')] # Remove duplicates from the dataset
-        if 'monthly2daily' in opts:
+        if 'monthly2daily' in dataset.opts:
             #print('INFO: Monthly data converted to daily')
             df[df.columns[0]] = pd.to_numeric(df[df.columns[0]],errors='coerce') #convert values to floats
             df = ConvertMonthlyToDaily(df)
-        df = df[~df.index.isnull()]
+        df = df[not df.index.isna()]
         df.columns = ['USBR | ' + dataset.external_id + ' | ' + dataset.param_code + ' | ' +
                       str(dataset.display_unit).upper()]
         df = df.round(3)
@@ -69,4 +71,13 @@ class Dataloader(object):
       return pd.Series()
 
 
+def ConvertMonthlyToDaily(df: pd.DataFrame):
+    # resample by filling forward
+    df_daily = pd.DataFrame(df[df.columns[0]].resample('D').ffill())
+    # get max day array
+    df_daily_max_days = df_daily.index.days_in_month
+    # divide monthly value by n-days - assumes monthly value is accumulated uniformly during the month
+    df_daily[df_daily.columns[0]] = df_daily[df_daily.columns[0]] / df_daily_max_days
+
+    return df_daily
 
